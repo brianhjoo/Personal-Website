754843cdab5c154708b108075a8d8ad8f813ceba
Brian Joo <brianhjoo@gmail.com>
2023-01-28 16:17:43 -0800

    start making modals

---
+++ script.js
@@ -0,0 +1,7 @@
+const modal = document.querySelector('#modal');
+const xBtn = document.querySelector('#x');
+
+const aboutLink = document.querySelector('#about-link');
+const projectsLink = document.querySelector('#projects-link');
+const contactLink = document.querySelector('#contact-link');
+
