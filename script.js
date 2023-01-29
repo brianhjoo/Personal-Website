86dd68bc2d7412e2f5ec5285d014297780e3dda6
Brian Joo <brianhjoo@gmail.com>
2023-01-28 18:48:38 -0800

    modals are set up without content

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const wrapper = document.querySelector('#wrapper');
 const modal = document.querySelector('#modal');
 const xBtn = document.querySelector('#x');
 
@@ -14,23 +15,27 @@ let currPage = '';
 aboutLink.addEventListener('click', e => {
   modal.style.display = 'block';
   aboutContent.style.display = 'block';
+  wrapper.style.filter = 'blur(0.2rem)';
   currPage = 'about';
 });
 
 projectsLink.addEventListener('click', e => {
   modal.style.display = 'block';
   projectsContent.style.display = 'block';
+  wrapper.style.filter = 'blur(0.2rem)';
   currPage = 'projects';
 });
 
 contactLink.addEventListener('click', e => {
   modal.style.display = 'block';
   contactContent.style.display = 'block';
+  wrapper.style.filter = 'blur(0.2rem)';
   currPage = 'contact';
 });
 
 xBtn.addEventListener('click', e => {
   modal.style.display = 'none';
+  wrapper.style.filter = 'none';
   if (currPage === 'about') {
     aboutContent.style.display = 'none';
   }
@@ -39,5 +44,21 @@ xBtn.addEventListener('click', e => {
   }
   else {
     contactContent.style.display = 'none';
+  }
+});
+
+window.addEventListener('click', e => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+    wrapper.style.filter = 'none';
+    if (currPage === 'about') {
+      aboutContent.style.display = 'none';
+    }
+    else if (currPage === 'projects') {
+      projectsContent.style.display = 'none';
+    }
+    else {
+      contactContent.style.display = 'none';
+    }
   }
 });
